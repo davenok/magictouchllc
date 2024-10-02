bae4650052f09c12e749ea6455c37e3808a4874c
David Burklin <david.burklin@halliburton.com>
2024-10-02 14:18:37 -0500

    commit

---
+++ action.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Magic Touch Lawncare</title>
+<meta name="description" content="">
+<meta name="author" content="">
+</head>
+<body>
+    <?php
+    print "Your submission has been received.";
+    ?>
+</body>
+</html>
